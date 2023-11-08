5530102be7931613c41b95ba096fb71c51e76a2d
Ilyes Aljane <aljaneelyes07@gmail.com>
2023-11-08 17:36:34 +0100

    composant supprimeractivite remplis

--- src/app/back-office/supprimeractivite/supprimeractivite.component.ts
+++ src/app/back-office/supprimeractivite/supprimeractivite.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Activite } from '../classes/activite';
+import { ActiviteService } from '../services/activite.service';
 
 @Component({
   selector: 'app-supprimeractivite',
@@ -6,6 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./supprimeractivite.component.css']
 })
 export class SupprimeractiviteComponent {
+  lesactivites!:Activite[]
 
+  constructor(private activiteService:ActiviteService){}
+
+  // onsupprimer(id:string){
+  //   this.activiteService.deleteActivite(Number(id)).subscribe(
+  //     ()=>this.lesactivites=this.lesactivites.filter(elt=>elt.id!=Number(id))
+  //   )
+  // }
+
+  onsupprimer(id: string) {
+    this.activiteService.deleteActivite(Number(id)).subscribe(
+      () => {
+        // Vérifiez si lesactivites est défini avant de filtrer
+        if (this.lesactivites) {
+          this.lesactivites = this.lesactivites.filter(elt => elt.id !== Number(id));
+        }
+      }
+    );
+  }
   
 }
