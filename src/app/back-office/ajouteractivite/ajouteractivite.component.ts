10e1cdaf4a1376af8e78fb1589aa3f58c5e753b1
Ilyes Aljane <aljaneelyes07@gmail.com>
2023-11-08 17:29:17 +0100

    composant ajoutercomposant remplis

--- src/app/back-office/ajouteractivite/ajouteractivite.component.ts
+++ src/app/back-office/ajouteractivite/ajouteractivite.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Activite } from '../classes/activite';
+import { ActiviteService } from '../services/activite.service';
+import { Details } from '../classes/details';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-ajouteractivite',
@@ -7,4 +10,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./ajouteractivite.component.css']
 })
 export class AjouteractiviteComponent {
+  lesactivites!:Activite[];
+
+  constructor(private activiteService:ActiviteService){}
+
+  nouvelId!: number;
+  nouveauTitre!: string;
+  nouvelleimage!: string;
+  nouvelleevaluation!:number;
+  dispo!:boolean;
+  nouvelledate!:Date;
+  nouvelintervenant!:string;
+  nouvelendroit!:string;
+  nouvelprerequis!:string;
+
+  onajouter(){
+    const details:Details={
+    intervenant: this.nouvelintervenant,
+    endroit: this.nouvelendroit,
+    prerequis: this.nouvelprerequis
+  };
+
+    const nouvelleActivite: Activite = {
+      id: this.nouvelId, 
+      titre: this.nouveauTitre,
+      image: this.nouvelleimage,
+      evaluation: this.nouvelleevaluation,
+      disponible: this.dispo,
+      date:this.nouvelledate,
+      details:[details]
+    };
+    this.activiteService.addActivite(nouvelleActivite).subscribe(
+      (data)=>{
+        this.lesactivites.push(data)
+        this.lesactivites.length++;
+      }
+    )
+  }
+
+
+//   constructor(private activiteService:ActiviteService){
+//     this.lesactivites=this.activiteService.getActivite();
+//   }
+
+// ajouter() {
+//   const details:Details={
+//     intervenant: this.nouvelintervenant,
+//     endroit: this.nouvelendroit,
+//     prerequis: this.nouvelprerequis};
+//   const nouvelleActivite: Activite = {
+//     id: this.nouvelId, 
+//     titre: this.nouveauTitre,
+//     image: this.nouvelleimage,
+//     evaluation: this.nouvelleevaluation,
+//     disponible: this.dispo,
+//     date:this.nouvelledate,
+//     details:[details]
+//   };
+//   // this.activiteService.ajouterActivite(nouvelleActivite);
+//   this.lesactivites.push(nouvelleActivite);
+// }
+
+ 
+//   ajouter(id:string,titre:string,image: string,evaluation:string,disponible: string,date: string,intervenant:string,endroit:string,prerequis:string){
+
+//   let details = new Details(intervenant, endroit, prerequis);
+//   let disponibleBoolean = disponible==='true' ;
+//   let dateObject = new Date(date);
+
+//   this.activiteService.getnouvelactivite(parseInt(id), titre, image,parseInt(evaluation),disponibleBoolean, dateObject, [details]);
+// }
+
+
 }
